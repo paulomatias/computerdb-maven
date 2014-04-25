b068b1622ac325b37132447111628cc2b894adee
Paulo Matias <pmatias@excilys.com>
2014-04-25 15:52:50 +0200

    Jax-RS

--- webservices/src/main/java/com/excilys/webservices/ComputerWebService.java
+++ webservices/src/main/java/com/excilys/webservices/ComputerWebService.java
@@ -2,21 +2,29 @@ package com.excilys.webservices;
 
 import java.util.List;
 
-import javax.jws.WebMethod;
-import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.excilys.domain.Computer;
+import com.excilys.mapper.DTOMapper;
 import com.excilys.service.ComputerService;
 
-@WebService
+@Component
+@Path("/webservices")
 public class ComputerWebService {
 
 	@Autowired
-	ComputerService computerService;
+	public ComputerService computerService;
 
-	@WebMethod
+	@Autowired
+	public DTOMapper dtoMapper;
+
+	@GET
+	@Produces("application/xml")
 	public List<Computer> findAll() {
 		return computerService.findAll();
 	}
